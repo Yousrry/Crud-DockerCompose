3f8c444ecedfb11edb5b26e43dac714557fb1442
Yousrry <yousry.arch@gmail.com>
2024-10-22 04:09:24 +0300

    changes to script

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-container_name="crud-app2_db_1"
+container_name=$(docker ps --filter "name=db" --format "{{.Names}}")
 
 # Check if container is running
 if [ $(docker inspect -f '{{.State.Running}}' $container_name) == "true" ]; then
     echo "$container_name is running."
 else
     echo "$container_name is not running. Restarting..."
+    cd /var/lib/jenkins/workspace/Crud-app
     docker-compose up -d
 fi
-
+~      
